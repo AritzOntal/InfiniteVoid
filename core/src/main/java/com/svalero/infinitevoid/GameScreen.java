a0357469707233212bd73dae33a35e20d3c0e9fc
Aritz Ontalvilla <aritzontal@gmail.com>
2026-03-23 03:29:31 +0100

    juego ejemplo funciones principales

--- core/src/main/java/com/svalero/infinitevoid/GameScreen.java
+++ core/src/main/java/com/svalero/infinitevoid/GameScreen.java
@@ -7,6 +7,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 
@@ -14,25 +15,32 @@ import com.badlogic.gdx.utils.Array;
 public class GameScreen implements Screen {
 
     private SpriteBatch batch;
-    private Texture player;
-    private Texture asteroid;
+    private Texture playerTexture;
+    private Texture asteroidTexture;
     private Vector2 playerPosition;
-    private Array<Vector2> asteriods;
+    private Array<Vector2> asteriodsPositions;
+    private Array<Rectangle> asteroidRectangels;
+    private Rectangle playerRectangle;
+
+    private float asteroidTimer;
+    private float spawnInterval;
 
 
     @Override
     public void show() {
-        player = new Texture(Gdx.files.internal("Ship2.png"));
-        asteroid = new Texture(Gdx.files.internal("asteroid.png"));
-        batch = new SpriteBatch();
+        playerTexture = new Texture(Gdx.files.internal("Ship2.png"));
         playerPosition = new Vector2(100, 100);
-        asteriods = new Array<>();
+        //RECTANGULO DE COLISION
+        playerRectangle = new Rectangle(
+            playerPosition.x, playerPosition.y, playerTexture.getWidth(), playerTexture.getHeight());
 
-        //METEMOS TODOS LOS ASTEROIDES EN EL ARRAY
-        for (int i = 0; i < 8; i++) {
-            Vector2 asteroidPosition = new Vector2(MathUtils.random(0, 1024), MathUtils.random( 768));
-            asteriods.add(asteroidPosition);
-        }
+        asteroidTexture = new Texture(Gdx.files.internal("asteroid.png"));
+
+        //INICIALIZAMOS LOS ARRAYS
+        asteriodsPositions = new Array<>();
+        asteroidRectangels = new Array<>();
+
+        batch = new SpriteBatch();
     }
 
     @Override
@@ -40,26 +48,72 @@ public class GameScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        asteroidTimer += delta;
+        spawnInterval = 1;
+
+        //TODO CONTROLAR VELOCIDAD QUE SE GENERAN LOS ASTEROIDES
+        if(asteroidTimer >= spawnInterval) {
+            //CREAMOS UNA POSICION CADA VEZ
+            Vector2 asteroidPosition = new Vector2(MathUtils.random(0, 1024), 768);
+            //Y LA AÑADIMOS AL ARRAY
+            asteriodsPositions.add(asteroidPosition);
+
+            //CREAMOS RECTANGLES CON LA POSICION (LA ANTERIOR) Y TAMAÑO DEL LA TEXTURA (DEL TEXTURE CREADO EN SHOW)
+            asteroidRectangels.add(
+                new Rectangle(asteroidPosition.x, asteroidPosition.y, asteroidTexture.getWidth(), asteroidTexture.getHeight()));
+
+            asteroidTimer = 0;
+        }
+
         batch.begin();
 
-        for (Vector2 asteriodPosition : asteriods) {}
-        batch.draw(player, playerPosition.x, playerPosition.y);
+        batch.draw(playerTexture, playerPosition.x, playerPosition.y);
+
+        //PINTAMOS LOS ASTEROIDES
+        for (Vector2 asteriodPosition : asteriodsPositions) {
+            batch.draw(asteroidTexture, asteriodPosition.x, asteriodPosition.y);
+        }
+
         batch.end();
 
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) ) {
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
             playerPosition.x -= 10;
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) ) {
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
             playerPosition.x += 10;
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.UP) ) {
+        if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
             playerPosition.y += 10;
         }
 
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) ) {
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
             playerPosition.y -= 10;
+        }
+
+        //ACTUALIZAMOS LA POSICION DEL RECTAGULO EN BASE LA DE LA TEXTURA
+        playerRectangle.setPosition(playerPosition.x, playerPosition.y);
+
+        //MOVEMOS ASTEROIDES JUNTO A RECTANGELS
+        for (int i = 0; i < asteroidRectangels.size; i++) {
+            asteriodsPositions.get(i).y -= 100 * delta;
+            asteroidRectangels.get(i).y = asteriodsPositions.get(i).y;
+        }
+
+
+        //COMPROBAMOS SI A COLISIONADO (HACEMOS BUCLE HACIA ATRAS PARA QUE FUNCIONE) Y DE PASO BORRAMOS CUANDO SALGA DE LA PANTALLA EL ASTEROIDE)
+        for (int i = asteroidRectangels.size - 1; i >= 0; i--) {
+            //pillamos el ultimo rectangulo cada vez
+            Rectangle asteroid = asteroidRectangels.get(i);
+
+            if (asteroid.overlaps(playerRectangle) || asteroid.y < -asteroidTexture.getHeight()) {
+                // Borramos el rectángulo de colisión
+                asteroidRectangels.removeValue(asteroid, true);
+
+                //BORRAMOS TAMBIEN LA POSICIÓN
+                asteriodsPositions.removeIndex(i);
+            }
         }
     }
 
@@ -84,6 +138,8 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-
+        asteroidTexture.dispose();
+        batch.dispose();
+        asteriodsPositions.clear();
     }
 }
